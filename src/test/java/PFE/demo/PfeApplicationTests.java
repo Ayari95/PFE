01e7cd14a21e96d8c21a93d8b6a8da0ffc3cb495
yassine <yassineayari95@gmail.com>
2024-03-19 11:01:42 +0100

    Initial commit

---
+++ src/test/java/PFE/demo/PfeApplicationTests.java
@@ -0,0 +1,13 @@
+package PFE.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
